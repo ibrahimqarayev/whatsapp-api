19ed3209400f672780010c50853eee23b80997a2
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-01 23:12:38 +0400

    Create JwtConstant

---
+++ src/main/java/az/whatsapp/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package az.whatsapp.config;
+
+public class JwtConstant {
+
+    public static final String JWT_HEADER = "Authorization";
+    public static final String SECRET_KEY = "Wud?AB3mONU?ykG-aNQuu5vJSCHpfbC4GFKjEvkYh31zTIT1pGVmKW5XKxXOAZmjIFyWvFke";
+
+}
