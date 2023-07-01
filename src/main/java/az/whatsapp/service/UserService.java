010005363ec1053aa74639b0534a900fb4a3b192
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-01 23:25:28 +0400

    Create UserService

---
+++ src/main/java/az/whatsapp/service/UserService.java
@@ -0,0 +1,18 @@
+package az.whatsapp.service;
+
+import az.whatsapp.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
+
+}
