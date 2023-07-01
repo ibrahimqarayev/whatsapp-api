64206f1d1984f0ab33c7e0a529e5edf483925f10
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-01 23:48:25 +0400

    Create GlobalException

---
+++ src/main/java/az/whatsapp/exception/GlobalException.java
@@ -0,0 +1,20 @@
+package az.whatsapp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<ErrorDetail> userExceptionHandler(UserException ex, WebRequest req) {
+        ErrorDetail errorDetail = new ErrorDetail(ex.getMessage(), req.getDescription(false), LocalDateTime.now());
+        return new ResponseEntity<ErrorDetail>(errorDetail, HttpStatus.BAD_REQUEST);
+    }
+
+}
