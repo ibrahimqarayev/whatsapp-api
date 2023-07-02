eb6fcc45ca06f937f70631014d60b126d57c3c3f
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-02 23:37:45 +0400

    Add method

--- src/main/java/az/whatsapp/service/UserService.java
+++ src/main/java/az/whatsapp/service/UserService.java
@@ -33,5 +33,9 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
 
 }
