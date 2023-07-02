878f63cc267d83c86b3b5699042970c76d708d4a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-02 23:31:57 +0400

    Add method

--- src/main/java/az/whatsapp/service/UserService.java
+++ src/main/java/az/whatsapp/service/UserService.java
@@ -1,7 +1,11 @@
 package az.whatsapp.service;
 
+import az.whatsapp.exception.UserException;
+import az.whatsapp.model.User;
 import az.whatsapp.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,7 +16,13 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-
+    public User findById(Integer id) {
+        Optional<User> opt = userRepository.findById(id);
+        if (opt.isPresent()) {
+            return opt.get();
+        }
+        throw new UserException("User not found");
+    }
 
 
 }
