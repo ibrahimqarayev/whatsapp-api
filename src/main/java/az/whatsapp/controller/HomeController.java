121e6b497c49b972e05dce982e6fd48ad4baf4b3
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-02 23:04:52 +0400

    Add method

--- src/main/java/az/whatsapp/controller/HomeController.java
+++ src/main/java/az/whatsapp/controller/HomeController.java
@@ -1,7 +1,16 @@
 package az.whatsapp.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> homeController() {
+        return new ResponseEntity<String>("Welcome to our whatsapp api using Spring Boot", HttpStatus.OK);
+    }
+
 }
