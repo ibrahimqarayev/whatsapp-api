f60067e29de0ee16d2dbfa66744eda63297aa51f
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-03 18:31:01 +0400

    Created ApiResponse

---
+++ src/main/java/az/whatsapp/response/ApiResponse.java
@@ -0,0 +1,11 @@
+package az.whatsapp.response;
+
+public class ApiResponse {
+    private String message;
+    private boolean status;
+
+    public ApiResponse(String message, boolean status) {
+        this.message = message;
+        this.status = status;
+    }
+}
