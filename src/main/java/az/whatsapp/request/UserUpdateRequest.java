0c838d0a7e24bb5e572d4775379a3176b28f9e0b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-03 18:36:43 +0400

    Add method

--- src/main/java/az/whatsapp/request/UserUpdateRequest.java
+++ src/main/java/az/whatsapp/request/UserUpdateRequest.java
@@ -12,4 +12,20 @@ public class UserUpdateRequest {
         this.fullName = fullName;
         this.profilePicture = profilePicture;
     }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
 }
