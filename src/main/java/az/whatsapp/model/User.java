84237edbc8837af95e543f36b50412a6815f61b6
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-03 18:32:08 +0400

    Add method

--- src/main/java/az/whatsapp/model/User.java
+++ src/main/java/az/whatsapp/model/User.java
@@ -25,4 +25,44 @@ public class User {
         this.profilePicture = profilePicture;
         this.password = password;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
