1bd74a77e5e5fc093aa7769b43970af83715a7a4
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-04 17:00:28 +0400

    Create SingleChatRequest

---
+++ src/main/java/az/whatsapp/request/SingleChatRequest.java
@@ -0,0 +1,20 @@
+package az.whatsapp.request;
+
+public class SingleChatRequest {
+    private Integer userId;
+
+    public SingleChatRequest() {
+    }
+
+    public SingleChatRequest(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
